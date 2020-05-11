fc49693b4ef36ed32327fcad29e8ff36c9a19cb6
gusflopes <gusflopes86@gmail.com>
2020-05-11 11:48:51 -0400

    feat: upload file

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -101,7 +101,10 @@ const Dashboard: React.FC = () => {
               {transactions.map(transaction => (
                 <tr key={transaction.id}>
                   <td className="title">{transaction.title}</td>
-                  <td className="income">{transaction.formattedValue}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {transaction.formattedValue}
+                  </td>
                   <td>{transaction.category.title}</td>
                   <td>20/04/2020</td>
                 </tr>
